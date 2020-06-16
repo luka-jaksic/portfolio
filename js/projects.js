a9e45bf71f92462dfa484c47fb6306f278539366
luka-jaksic <66182144+luka-jaksic@users.noreply.github.com>
2020-06-16 04:20:57 +0200

    Adding all files Initial commit

---
+++ js/projects.js
@@ -0,0 +1,18 @@
+var TitleText = document.getElementById("name");
+
+function changeCurrentProject(whichButton){
+    switch(whichButton){
+        case 1:
+            TitleText.textContent = "KysLOL";
+            break;
+        case 2:
+            TitleText.textContent = "KysLOL";
+            break;
+        case 3:
+            TitleText.textContent = "KysLOL";
+            break;
+        case 4:
+            TitleText.textContent = "KysLOL";
+            break;
+    }
+}
